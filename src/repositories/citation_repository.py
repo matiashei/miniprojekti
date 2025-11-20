336273e5a4f0a1a95639fd3631c3e3291927786d
karorot <174889933+karorot@users.noreply.github.com>
2025-11-20 12:35:43 +0200

    Add citation deletion

--- src/repositories/citation_repository.py
+++ src/repositories/citation_repository.py
@@ -3,52 +3,49 @@ from config import db, app
 
 from entities.citation import Citation
 
-def get_citations():
-    result = db.session.execute(text("SELECT id, content FROM citations"))
-    citations = result.fetchall()
-    return [Citation(citation[0], citation[1]) for citation in citations]
-
-def create_citation(content):
-    sql = text("INSERT INTO citations (content) VALUES (:content)")
-    db.session.execute(sql, { "content": content })
-    db.session.commit()
-
-def create_book_citation(title, author, publisher, isbn, year):
+def create_book_citation(citation_type, title, author, publisher, isbn, year):
     with app.app_context():
         sql = text("""INSERT INTO citations
             (type, title, author, publisher, isbn, year)
             VALUES ('book', :title, :author, :publisher, :isbn, :year)
         """)
 
-        db.session.execute(sql, { "title": title, "author": author, "publisher": publisher,
-                                "isbn": isbn, "year": year })
+        db.session.execute(sql, { "type": citation_type, "title": title, "author": author,
+                                "publisher": publisher, "isbn": isbn, "year": year })
         db.session.commit()
 
-def create_inproceedings_citation(title, author, booktitle, year):
+def create_inproceedings_citation(citation_type, title, author, booktitle, year):
     with app.app_context():
+        citation_type = "inproceedings"
         sql = text("""INSERT INTO citations
         (type, title, author, booktitle, year)
         VALUES ('inproceedings', :title, :author, :booktitle, :year)
         """)
 
-        db.session.execute(sql, { "title": title, "author": author, "booktitle": booktitle,
-                                "year": year })
+        db.session.execute(sql, { "type": citation_type, "title": title, "author": author,
+                                "booktitle": booktitle, "year": year })
         db.session.commit()
 
-def create_article_citation(title, author, journal, year):
+def create_article_citation(citation_type, title, author, journal, year):
     with app.app_context():
         sql = text("""INSERT INTO citations
             (type, title, author, journal, year)
             VALUES ('article', :title, :author, :journal, :year)
         """)
 
-        db.session.execute(sql, { "title": title, "author": author, "journal": journal,
-                                "year": year })
+        db.session.execute(sql, { "type": citation_type, "title": title, "author": author,
+                                "journal": journal, "year": year })
         db.session.commit()
 
-# Temporary function to get book citations for front page listing
+def delete_citation(citation_id):
+    with app.app_context():
+        sql = text("DELETE FROM citations WHERE id = :id")
+        db.session.execute(sql, {"id": citation_id})
+        db.session.commit()
+
+# Temporary function to get all citations for front page listing
 def get_book_citations():
-    result = db.session.execute(text("SELECT title, author, publisher, isbn, year " \
+    result = db.session.execute(text("SELECT id, type, title, author, publisher, isbn, year " \
     "FROM citations"))
     book_citations = result.fetchall()
     return book_citations
