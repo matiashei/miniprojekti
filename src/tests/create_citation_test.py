020cdf3966d3abe8ef4e2794e4a45d4b3e99c309
karorot <174889933+karorot@users.noreply.github.com>
2025-11-25 20:54:07 +0200

    Add unittests for creating different types of citations

---
+++ src/tests/create_citation_test.py
@@ -0,0 +1,65 @@
+import unittest
+from unittest.mock import patch, ANY
+
+from repositories.citation_repository import (
+    create_book_citation,
+    create_inproceedings_citation,
+    create_article_citation
+)
+
+class TestCreateCitation(unittest.TestCase):
+    def setUp(self):
+        self.book_citation = ("book", "Title", "Author", "Publisher", "1234", "2023")
+        self.inproceedings_citation = ("inproceedings", "Title", "Author", "Booktitle", "2023")
+        self.article_citation = ("article", "Title", "Author", "Journal", "2023")
+
+    @patch("repositories.citation_repository.db")
+    def test_create_valid_book_citation(self, mock_db):
+        create_book_citation(*self.book_citation)
+
+        mock_db.session.execute.assert_called_once_with(ANY, {
+            "citation_type": self.book_citation[0],
+            "title": self.book_citation[1],
+            "author": self.book_citation[2],
+            "publisher": self.book_citation[3],
+            "isbn": self.book_citation[4],
+            "year": self.book_citation[5]
+            }
+        )
+        mock_db.session.commit.assert_called_once()
+
+    @patch("repositories.citation_repository.db")
+    def test_create_valid_inproceedings_citation(self, mock_db):
+        create_inproceedings_citation(*self.inproceedings_citation)
+
+        mock_db.session.execute.assert_called_once_with(ANY, {
+            "citation_type": self.inproceedings_citation[0],
+            "title": self.inproceedings_citation[1],
+            "author": self.inproceedings_citation[2],
+            "booktitle": self.inproceedings_citation[3],
+            "year": self.inproceedings_citation[4]
+            }
+        )
+        mock_db.session.commit.assert_called_once()
+
+    @patch("repositories.citation_repository.db")
+    def test_create_valid_article_citation(self, mock_db):
+        create_article_citation(*self.article_citation)
+
+        mock_db.session.execute.assert_called_once_with(ANY, {
+            "citation_type": self.article_citation[0],
+            "title": self.article_citation[1],
+            "author": self.article_citation[2],
+            "journal": self.article_citation[3],
+            "year": self.article_citation[4]
+            }
+        )
+        mock_db.session.commit.assert_called_once()
+
+    @patch("repositories.citation_repository.db")
+    def test_create_consecutive_citations(self, mock_db):
+        create_book_citation(*self.book_citation)
+        create_article_citation(*self.article_citation)
+        create_inproceedings_citation(*self.inproceedings_citation)
+
+        self.assertEqual(mock_db.session.execute.call_count, 3)
--- src/tests/util_test.py
+++ src/tests/util_test.py
@@ -1,7 +1,6 @@
 import unittest
 
-from attr import validate
-from util import validate_book, UserInputError
+from util import UserInputError, validate_book, validate_inproceedings, validate_article
 
 class TestValidateBook(unittest.TestCase):
 
@@ -55,3 +54,95 @@ class TestValidateBook(unittest.TestCase):
     def test_too_low_year(self):
         with self.assertRaises(UserInputError):
             validate_book("Title", "Author", "Publisher", "123", "-1")
+
+
+class TestValidateInproceedings(unittest.TestCase):
+
+    def test_valid_inproceedings(self):
+        validate_inproceedings("Title", "Author", "Book Title", "2025")
+
+    def test_empty_title(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("", "Author", "Book Title", "2025")
+
+    def test_title_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings(" ", "Author", "Book Title", "2025")
+
+    def test_too_long_title(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("x" * 200, "Author", "Book Title", "2025")
+
+    def test_author_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("Title", " ", "Book Title", "2025")
+
+    def test_too_long_author(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("Title", "x" * 76, "Book Title", "2025")
+
+    def test_boobktitle_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("Title", "Author", " ", "2025")
+
+    def test_too_long_booktitle(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("Title", "Author", "x" * 76, "2025")
+
+    def test_year_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("Title", "Author", "Booktitle", " ")
+
+    def test_too_high_year(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("Title", "Author", "Booktitle", "2026")
+
+    def test_too_low_year(self):
+        with self.assertRaises(UserInputError):
+            validate_inproceedings("Title", "Author", "Booktitle", "-1")
+
+
+class TestValidateArticle(unittest.TestCase):
+
+    def test_valid_article(self):
+        validate_article("Title", "Author", "Journal", "2025")
+
+    def test_empty_title(self):
+        with self.assertRaises(UserInputError):
+            validate_article("", "Author", "Journal", "2025")
+
+    def test_title_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_article(" ", "Author", "Journal", "2025")
+
+    def test_too_long_title(self):
+        with self.assertRaises(UserInputError):
+            validate_article("x" * 200, "Author", "Journal", "2025")
+
+    def test_author_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_article("Title", " ", "Journal", "2025")
+
+    def test_too_long_author(self):
+        with self.assertRaises(UserInputError):
+            validate_article("Title", "x" * 76, "Journal", "2025")
+
+    def test_journal_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_article("Title", "Author", " ", "2025")
+
+    def test_too_long_journal(self):
+        with self.assertRaises(UserInputError):
+            validate_article("Title", "Author", "x" * 76, "2025")
+
+    def test_year_is_space(self):
+        with self.assertRaises(UserInputError):
+            validate_article("Title", "Author", "Journal", " ")
+
+    def test_too_high_year(self):
+        with self.assertRaises(UserInputError):
+            validate_article("Title", "Author", "Journal", "2026")
+
+    def test_too_low_year(self):
+        with self.assertRaises(UserInputError):
+            validate_article("Title", "Author", "Journal", "-1")
